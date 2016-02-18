f57b7850e7714256ff522f015e856747e10f9736
RealMagoj <RealMagoj@users.noreply.github.com>
2016-02-17 23:13:26 -0500

    contact list app v.3 - complete exercise

--- contact.rb
+++ contact.rb
@@ -1,75 +1,16 @@
-require 'byebug'
-require 'csv'
-require 'pg'
+require 'active_record'
 
-class Contact
+class Contact < ActiveRecord::Base
 
-  attr_accessor :id, :name, :email
-  
-  def initialize(id=nil, name, email)
-    @id = id
-    @name = name
-    @email = email
-  end
-
-  def save
-    if self.id == nil
-      new_contact = Contact.connection.exec_params('INSERT INTO contacts (name, email) VALUES ($1, $2)', [self.name, self.email])     
-    else
-      update_contact = Contact.connection.exec_params('UPDATE contacts SET name = $1, email = $2 WHERE id = $3::int', [self.name, self.email, self.id])
-    end
-  end
-
-  def destroy
-    delete_contact = Contact.connection.exec_params('DELETE FROM contacts WHERE id = $1::int', [self.id])
-  end
+  belongs_to :contact_list
 
   class << self
 
-    def connection
-      conn = PG.connect(
-        host: 'localhost',
-        dbname: 'contact_list',
-        user: 'development',
-        password: 'development'
-        )
-    end
-
-    def all
-      new_contact_instance = connection.exec('SELECT * FROM contacts ORDER BY id')
-      contacts = []
-      new_contact_instance.to_a.each do |contact|
-        contact = Contact.new(contact['id'].to_i, contact['name'], contact['email'])
-        contacts << contact
-      end
-      contacts
-    end
-
-
-    def create(name, email)
-      new_contact = Contact.new(name, email)
-      new_contact.save
-      new_contact
-    end
-
-    def find(id)
-      contact = connection.exec_params('SELECT * FROM contacts WHERE id = $1::int', [id.to_i])
-      if contact.to_a != []
-        new_contact = Contact.new(contact.to_a[0]['id'].to_i, contact.to_a[0]['name'], contact.to_a[0]['email'])
-        new_contact
-      end
-    end
-    
     def search(term)
       if term
-        contact_list = []
-        contacts = connection.exec_params('SELECT * FROM contacts WHERE name LIKE $1
-                                            OR email LIKE $1', ["%#{term}%"])
-        contacts.to_a.each do |contact|
-          contact = Contact.new(contact['id'].to_i, contact['name'], contact['email'])
-          contact_list << contact
-        end
-        contact_list
+        Contact.where("name LIKE ? or email LIKE ?", "%#{term}%", "%#{term}%")
+      else
+        puts "Invalid command"
       end
     end
 
--- contact_list.rb
+++ contact_list.rb
@@ -1,84 +1,121 @@
 require './contact.rb'
+require 'active_record'
 require 'pry'
 
-class ContactList
-  
-  input = ARGV
-  
-  case input[0]
-  when "list"
-    contacts = Contact.all
-    contacts.each do |contact|
-      puts "#{contact.id}: #{contact.name} (#{contact.email})"
-    end
-    puts "-----------------"
-    puts "#{contacts.length} records total"
-  when "new"
-    puts "What is the full (First Last) name of the contact you wish to add?"
-    name = STDIN.gets.chomp
-    puts "What is the e-mail (name@email.com) address of the contact you wish to add?"
-    email = STDIN.gets.chomp
-    new_contact = Contact.create(name, email)
-    puts "#{new_contact.name} (#{new_contact.email}) contact was created successfully!"
-  when "show"
-    contact = Contact.find(input[1])
-    if contact
-        puts "#{contact.name}"
-        puts "(#{contact.email})"
-    else
-      puts "Contact Not Found!"
-    end
-  when "search"
-    contact_list = Contact.search(input[1])
-    if contact_list && contact_list.length > 0
-      contact_list.each do |contact|
-        puts "#{contact.name}"
-        puts "(#{contact.email})"
+class ContactList < ActiveRecord::Base
+
+  has_many :contacts
+
+  def self.initialize_schema
+    ActiveRecord::Schema.define do
+      create_table :contacts do |t|
+        t.column :name, :string
+        t.column :email, :string
+        t.timestamps null: false
       end
-    else
-      puts "Contact Not Found!"
     end
-  when "update"
-    contact = Contact.find(input[1])
-    if contact
-      puts "Which attribute would you like to update ('name' or 'email'):"
-      response = STDIN.gets.chomp
-      if
-        response == 'name'
-        puts "What is the new (First and Last) name?"
-        new_name = STDIN.gets.chomp
-        contact.name = new_name
-        contact.save
-        puts "Updated name to #{contact.name}!"
-      elsif
-        response == 'email'
-        puts "What is the new e-mail address?"
-        new_email = STDIN.gets.chomp
-        contact.email = new_email
-        contact.save
-        puts "Updated email to #{contact.email}!"
+  end
+  
+  def self.setup
+
+    ActiveRecord::Base.establish_connection(
+    adapter: 'postgresql',
+    database: 'contact_list',
+    username: 'development',
+    password: 'development',
+    host: 'localhost'
+    )
+
+    initialize_schema unless ActiveRecord::Base.connection.table_exists?(:contacts)
+    
+    input = ARGV
+    
+    case input[0]
+    when "list"
+      contacts = Contact.all.order(:id)
+      contacts.each do |contact|
+        puts "#{contact.id}: #{contact.name} (#{contact.email})"
+      end
+      puts "-----------------"
+      puts "#{contacts.length} records total"
+    when "new"
+      puts "What is the full (First Last) name of the contact you wish to add?"
+      name = STDIN.gets.chomp
+      puts "What is the e-mail (name@email.com) address of the contact you wish to add?"
+      email = STDIN.gets.chomp
+      new_contact = Contact.create(name: name, email: email)
+      puts "'#{new_contact.name} (#{new_contact.email})' contact was created successfully!"
+    when "show"
+      if !input[1]
+        puts "Invalid command"
       else
-        puts "Invalid input!"
+        if Contact.exists?(id: input[1])
+          contact = Contact.find(input[1])
+          puts "#{contact.name}"
+          puts "(#{contact.email})"
+        else
+          puts "Contact Not Found!"
+        end
+      end
+    when "search"
+      contact_list = Contact.search(input[1])
+      if contact_list && contact_list.length > 0
+        contact_list.each do |contact|
+          puts "#{contact.name}"
+          puts "(#{contact.email})"
+        end
+      else
+        puts "Contact Not Found!"
+      end
+    when "update"
+      if !input[1]
+        puts "Invalid command"
+      elsif
+        Contact.exists?(id: input[1])
+        contact = Contact.find(input[1])
+        puts "Which attribute would you like to update ('name' or 'email'):"
+        response = STDIN.gets.chomp
+        if
+          response == 'name'
+          puts "What is the new (First and Last) name?"
+          new_name = STDIN.gets.chomp
+          contact.name = new_name
+          contact.save
+          puts "Updated name to '#{contact.name}'!"
+        elsif
+          response == 'email'
+          puts "What is the new e-mail address?"
+          new_email = STDIN.gets.chomp
+          contact.email = new_email
+          contact.save
+          puts "Updated email to '#{contact.email}'!"
+        else
+          puts "Invalid command"
+        end
+      else
+        puts "Contact Not Found!"
+      end
+    when "delete"
+      if !input[1]
+        puts "Invalid command"
+      elsif Contact.exists?(id: input[1])
+        contact = Contact.find(input[1])
+        contact.destroy
+        puts "'#{contact.name} (#{contact.email})' has been deleted!"
+      else
+        puts "Contact Not Found!"
       end
     else
-      puts "Contact Not Found!"
+      puts "Here is a list of available commands:"
+      puts "    new     - Create a new contact"
+      puts "    list    - List all contacts"
+      puts "    show    - Show a contact"
+      puts "    search  - Search contacts"
+      puts "    update  - Update existing contact"
+      puts "    delete  - Delete existing contact"
     end
-  when "delete"
-    contact = Contact.find(input[1])
-    if contact
-      contact.destroy
-      puts "#{contact.name} (#{contact.email}) has been deleted!"
-    else
-      puts "Contact Not Found!"
-    end
-  else
-    puts "Here is a list of available commands:"
-    puts "    new     - Create a new contact"
-    puts "    list    - List all contacts"
-    puts "    show    - Show a contact"
-    puts "    search  - Search contacts"
-    puts "    update  - Update existing contact"
-    puts "    delete  - Delete existing contact"
   end
 
 end
+
+ContactList.setup
